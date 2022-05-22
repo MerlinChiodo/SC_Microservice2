3efa6aa4083c5d58bca6aca3c3e65f7ea0f73b1e
celinedridiger <celine.dridiger@fh-bielefeld.de>
2022-05-22 19:23:13 +0200

    added basic frontend pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Employee from "./pages/Employee";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+      <Router>
+              <Navbar/>
+              <main className='container mx-auto '>
+                  <Routes>
+                      <Route path='/' element={
+                              <>
+                                  <Home/>
+                              </>
+                      }/>
+                      <Route path='/home' element={<Home/>}/>
+                      <Route path='/employee' element={<Employee/>} />
+                  </Routes>
+              </main>
+      </Router>
+  )
 }
 
-export default App;
+export default App

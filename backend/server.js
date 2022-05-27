d85eb6ae89468d1f67c1bed80b51156fd7dc03ac
celinedridiger <celine.dridiger@fh-bielefeld.de>
2022-05-27 20:22:01 +0200

    edited requestOptions

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,9 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", async (req, res) =>{
+/*app.get("*", async (req, res) =>{
     res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
-})
+})*/
 
 app.use(indexRouter)
 

fbde66d2c5019bae2109a5eca6b97b26ea05e775
celinedridiger <celine.dridiger@fh-bielefeld.de>
2022-05-29 18:58:34 +0200

    switched to axios

--- frontend/src/eventController.js
+++ frontend/src/eventController.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const backendurl = "http://" + window.location.host +"/event/"
 //test: funktioniert lokal aber nicht auf dem server
 
@@ -7,18 +9,21 @@ export async function sendCalendarEntry(){
         cache: 'no-cache',
         credentials: 'same-origin',
         redirect: 'follow',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            event_id: 2002,
+        headers: { 'Content-Type': 'application/json' }
+    };
+    return await axios.post(backendurl + "sendCalendarEntry", {
+        event_id: 2002,
             event_name: "New Calendar Entry",
             service_name: "stadtbus",
             title: "Bus Event",
             short_description: "Smarte Events im Stadtbus",
             long_description: "Lorem Ipsum",
-            event_on: "22.05.2022" }),
-    };
-    const response = await fetch(backendurl + "sendCalendarEntry", requestOptions)
-    const data = await response.json()
+            event_on: "22.05.2022" }, requestOptions)
+        .then((response) => {
+            console.log(response);
+        }, (error) => {
+            console.log(error);
+        });
 }
 
 

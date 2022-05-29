987507d08d0a8beed8e61560f3fa90e9c8cde042
celinedridiger <celine.dridiger@fh-bielefeld.de>
2022-05-29 20:19:24 +0200

    trying to fix network error

--- frontend/src/eventController.js
+++ frontend/src/eventController.js
@@ -1,22 +1,27 @@
-import axios from 'axios'
 
 const backendurl = "http://" + window.location.host +"/event/"
 //test: funktioniert lokal aber nicht auf dem server
 
 export async function sendCalendarEntry(){
-    const requestOptions = {
+    fetch('/event/sendCalendarEntry', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    };
-    const response = await axios.post(backendurl + "sendCalendarEntry", {
-        event_id: 2002,
-        event_name: "New Calendar Entry",
-        service_name: "stadtbus",
-        title: "Bus Event",
-        short_description: "Smarte Events im Stadtbus",
-        long_description: "Lorem Ipsum",
-        event_on: "22.05.2022" }, requestOptions)
-    return response.data
+        headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+        body: JSON.stringify({
+            event_id: 2002,
+            event_name: "New Calendar Entry",
+            service_name: "stadtbus",
+            title: "Bus Event",
+            short_description: "Smarte Events im Stadtbus",
+            long_description: "Lorem Ipsum",
+            event_on: "22.05.2022" })
+    }).then(async (response) => {
+        if (response.ok) {
+            let result = await response.json();
+
+        }
+    }).catch(error => {
+            console.error(error);
+    })
 }
 
 

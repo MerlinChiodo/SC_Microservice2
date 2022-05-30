6dff842f5422dc79b257b003962983495c2c7797
celinedridiger <celine.dridiger@fh-bielefeld.de>
2022-05-30 13:09:29 +0200

    trying to fix network error

--- backend/controller/eventController.js
+++ backend/controller/eventController.js
@@ -2,5 +2,5 @@ const rabbitmq = require("../events/forum/calendar_event");
 
 exports.sendCalendarEvent = async (req, res) => {
     await rabbitmq.sendCalendarEntry(req, res)
-    return res.status(200)
+    return res.status(200).send({error: false, msg: 'event successfully sent'})
 }

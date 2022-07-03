45db354d9d03e56189ca586fcf13f0e58e9a051d
celinedridiger <celine.dridiger@fh-bielefeld.de>
2022-07-03 21:04:13 +0200

    fixed checkout button

--- frontend/src/components/Drawer.jsx
+++ frontend/src/components/Drawer.jsx
@@ -2,11 +2,13 @@ import DrawerItem from "./DrawerItem";
 import {Drawer} from "@mantine/core";
 import RouteContext from "../context/route/RouteContext";
 import {useContext} from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+
 
 function ShoppingCartDrawer() {
 
     const {cart_opened, setCart_opened, tickets} = useContext(RouteContext)
+    const navigate = useNavigate()
 
  /*   function calcSubtotal(){
         let tickets = JSON.parse(localStorage.getItem("tickets"));
@@ -17,6 +19,12 @@ function ShoppingCartDrawer() {
             }
         return sum
     }*/
+    const handleClick = () => {
+        if(tickets.length !== 0){
+            navigate("/login")
+        }
+
+    }
 
     return(
         <Drawer
@@ -34,8 +42,9 @@ function ShoppingCartDrawer() {
             </div>
             <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
                 <div className="mt-6">
-                    <Link to="/login"
-                       className="flex items-center justify-center rounded-md border border-transparent bg-neutral-content px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-neutral-content">Checkout</Link>
+                   <button onClick={handleClick} className="flex mx-auto items-center w-1/2 justify-center rounded-md border border-transparent bg-neutral-content px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-neutral-focus">
+                        Checkout
+                   </button>
                 </div>
                 <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                     <p>

4582e6e8d70d7b7088e5b39328d9861f7d865437
celinedridiger <celine.dridiger@fh-bielefeld.de>
2022-07-04 10:31:45 +0200

    TODO: fix scrollbar, removed dark mode label, fixed mapbox popup, added logo

--- frontend/src/pages/NewsletterForm.jsx
+++ frontend/src/pages/NewsletterForm.jsx
@@ -90,7 +90,7 @@ function NewsletterForm(){
                 />
             </div>
             <div className="mt-6">
-                <label htmlFor="long_description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Langbeschreibung</label>
+                <label htmlFor="long_description" className="block mb-2 text-sm font-medium text-gray-900">Langbeschreibung</label>
                 <textarea id="long_description"
                           rows="4"
                           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
